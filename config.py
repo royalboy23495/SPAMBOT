c485e2e80ca14801d8dd3be3ff2e13430e0f1749
royalboy23495 <165245336+royalboy23495@users.noreply.github.com>
2024-08-10 22:17:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,13 +10,13 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 # VALUES REQUIRED FOR XBOTS
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+API_ID = 29271553
+API_HASH = "bb67d176cb1f53eb1934bccf571b0b04"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
-BOT_TOKEN = getenv("BOT_TOKEN", default=None)
+BOT_TOKEN = getenv("BOT_TOKEN", default=7219043641:AAFAqAFMiuPMR7ZwRMXPOePYBGtQmxSptQE)
 BOT_TOKEN2 = getenv("BOT_TOKEN2", default=None)
 BOT_TOKEN3 = getenv("BOT_TOKEN3", default=None)
 BOT_TOKEN4 = getenv("BOT_TOKEN4", default=None)
